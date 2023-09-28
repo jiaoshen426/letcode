f6d653f632f60a3294d47f8cd54a5b665fb9d623
zezhou.shen <805057288@qq.com>
2023-09-28 14:27:45 +0800

    test

---
+++ letcode/src/com/jiaoshen/test/Test3.java
@@ -0,0 +1,13 @@
+package com.jiaoshen.test;
+
+/**
+ * @author jiaoshen
+ * @date 2023/9/12-9:07
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) System.out.println("Hello, Java!");
+        if (flag == true) System.out.println("Hello, JVM!");
+    }
+}
